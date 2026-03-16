e0e04da74a9632bed779226063a6f6fd3c6e57c0
summer-marie <summer.halsey0318@gmail.com>
2026-03-16 11:02:09 -0700

    Adds comments for plannign workflow

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # summer-marie.github.io
 Schedule Creator
+
+
+<!-- basic readme describing the functionality of the site  -->
+<!-- add functions and features as they are made - remove if no longer needed  -->
---
+++ script.js
@@ -0,0 +1,12 @@
+// handle button click actions
+    // add employee to local storage
+    // add employee to list of employees
+//  handle creating employee list
+    // handle grabbing employees from local storage
+    // handle rendering employee list
+// handle dynamic rendering of schedule table
+    // handle grabbing employees from list of employees
+    // handle rendering of schedule table
+// Local storage functions
+    // handle saving employees to local storage
+    // handle grabbing employees from local storage
