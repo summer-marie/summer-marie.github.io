7a8eb358a737cfc320506ed8959d4ad7fc385ba3
summer-marie <summer.halsey0318@gmail.com>
2026-03-16 11:49:57 -0700

    Adds js function to hide/show employee form

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // handle button click actions
+
+// handle showing/hiding add employee form
+const addEmployeeBtn = document.getElementById('add-employee-btn');
+const employeeForm = document.getElementById('employee-form');
+
+// toggle employee form visibility
+addEmployeeBtn.addEventListener('click', () => {
+    if (employeeForm.style.display === 'none' || employeeForm.style.display === '') {
+        employeeForm.style.display = 'block';
+    } else {
+        employeeForm.style.display = 'none';
+    }
+});
+
+
+
     // add employee to local storage
     // add employee to list of employees
+
 //  handle creating employee list
     // handle grabbing employees from local storage
     // handle rendering employee list
